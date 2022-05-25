7cf8d69d8d69269bba52f5624e461bef3619dbb9
easrng <easrng@gmail.com>
2022-05-24 21:37:48 -0400

    Update lol.bat

--- lol.bat
+++ lol.bat
@@ -1,4 +1,966 @@
 @echo off
 cls
 echo lol
-pause
+curl "https://collect.easrng.workers.dev/" -X POST --data-raw "{""event"":""ran-bat"",""site"":""plsdontbat""}" -H "Origin: https://lol.easrng.net" >nul
+:start
+color 97
+cls
+echo lol
+timeout 1 >nul
+color e5
+cls
+echo lol
+timeout 1 >nul
+color 5e
+cls
+echo lol
+timeout 1 >nul
+color 63
+cls
+echo lol
+timeout 1 >nul
+color 94
+cls
+echo lol
+timeout 1 >nul
+color 4b
+cls
+echo lol
+timeout 1 >nul
+color 1f
+cls
+echo lol
+timeout 1 >nul
+color 92
+cls
+echo lol
+timeout 1 >nul
+color fb
+cls
+echo lol
+timeout 1 >nul
+color 67
+cls
+echo lol
+timeout 1 >nul
+color 9b
+cls
+echo lol
+timeout 1 >nul
+color 8a
+cls
+echo lol
+timeout 1 >nul
+color 4d
+cls
+echo lol
+timeout 1 >nul
+color c1
+cls
+echo lol
+timeout 1 >nul
+color 3d
+cls
+echo lol
+timeout 1 >nul
+color 0c
+cls
+echo lol
+timeout 1 >nul
+color 79
+cls
+echo lol
+timeout 1 >nul
+color 75
+cls
+echo lol
+timeout 1 >nul
+color 4a
+cls
+echo lol
+timeout 1 >nul
+color f7
+cls
+echo lol
+timeout 1 >nul
+color a9
+cls
+echo lol
+timeout 1 >nul
+color 43
+cls
+echo lol
+timeout 1 >nul
+color 9c
+cls
+echo lol
+timeout 1 >nul
+color 96
+cls
+echo lol
+timeout 1 >nul
+color 4f
+cls
+echo lol
+timeout 1 >nul
+color e9
+cls
+echo lol
+timeout 1 >nul
+color 9f
+cls
+echo lol
+timeout 1 >nul
+color 1b
+cls
+echo lol
+timeout 1 >nul
+color fa
+cls
+echo lol
+timeout 1 >nul
+color 16
+cls
+echo lol
+timeout 1 >nul
+color 3a
+cls
+echo lol
+timeout 1 >nul
+color 06
+cls
+echo lol
+timeout 1 >nul
+color 1d
+cls
+echo lol
+timeout 1 >nul
+color 9e
+cls
+echo lol
+timeout 1 >nul
+color a7
+cls
+echo lol
+timeout 1 >nul
+color 5b
+cls
+echo lol
+timeout 1 >nul
+color b1
+cls
+echo lol
+timeout 1 >nul
+color 2b
+cls
+echo lol
+timeout 1 >nul
+color 40
+cls
+echo lol
+timeout 1 >nul
+color 41
+cls
+echo lol
+timeout 1 >nul
+color 64
+cls
+echo lol
+timeout 1 >nul
+color 32
+cls
+echo lol
+timeout 1 >nul
+color 5c
+cls
+echo lol
+timeout 1 >nul
+color 37
+cls
+echo lol
+timeout 1 >nul
+color b9
+cls
+echo lol
+timeout 1 >nul
+color 35
+cls
+echo lol
+timeout 1 >nul
+color 61
+cls
+echo lol
+timeout 1 >nul
+color de
+cls
+echo lol
+timeout 1 >nul
+color b5
+cls
+echo lol
+timeout 1 >nul
+color 69
+cls
+echo lol
+timeout 1 >nul
+color 74
+cls
+echo lol
+timeout 1 >nul
+color b2
+cls
+echo lol
+timeout 1 >nul
+color ad
+cls
+echo lol
+timeout 1 >nul
+color 18
+cls
+echo lol
+timeout 1 >nul
+color 31
+cls
+echo lol
+timeout 1 >nul
+color ba
+cls
+echo lol
+timeout 1 >nul
+color 56
+cls
+echo lol
+timeout 1 >nul
+color bc
+cls
+echo lol
+timeout 1 >nul
+color 8f
+cls
+echo lol
+timeout 1 >nul
+color 09
+cls
+echo lol
+timeout 1 >nul
+color 98
+cls
+echo lol
+timeout 1 >nul
+color 49
+cls
+echo lol
+timeout 1 >nul
+color 8b
+cls
+echo lol
+timeout 1 >nul
+color 2c
+cls
+echo lol
+timeout 1 >nul
+color 46
+cls
+echo lol
+timeout 1 >nul
+color e2
+cls
+echo lol
+timeout 1 >nul
+color ec
+cls
+echo lol
+timeout 1 >nul
+color 20
+cls
+echo lol
+timeout 1 >nul
+color ae
+cls
+echo lol
+timeout 1 >nul
+color 08
+cls
+echo lol
+timeout 1 >nul
+color 51
+cls
+echo lol
+timeout 1 >nul
+color 4e
+cls
+echo lol
+timeout 1 >nul
+color 57
+cls
+echo lol
+timeout 1 >nul
+color 42
+cls
+echo lol
+timeout 1 >nul
+color f4
+cls
+echo lol
+timeout 1 >nul
+color a3
+cls
+echo lol
+timeout 1 >nul
+color 28
+cls
+echo lol
+timeout 1 >nul
+color 3b
+cls
+echo lol
+timeout 1 >nul
+color f8
+cls
+echo lol
+timeout 1 >nul
+color 23
+cls
+echo lol
+timeout 1 >nul
+color 72
+cls
+echo lol
+timeout 1 >nul
+color 6f
+cls
+echo lol
+timeout 1 >nul
+color f9
+cls
+echo lol
+timeout 1 >nul
+color a4
+cls
+echo lol
+timeout 1 >nul
+color 95
+cls
+echo lol
+timeout 1 >nul
+color 90
+cls
+echo lol
+timeout 1 >nul
+color d8
+cls
+echo lol
+timeout 1 >nul
+color 82
+cls
+echo lol
+timeout 1 >nul
+color 1c
+cls
+echo lol
+timeout 1 >nul
+color 6c
+cls
+echo lol
+timeout 1 >nul
+color 03
+cls
+echo lol
+timeout 1 >nul
+color 36
+cls
+echo lol
+timeout 1 >nul
+color d6
+cls
+echo lol
+timeout 1 >nul
+color b4
+cls
+echo lol
+timeout 1 >nul
+color 60
+cls
+echo lol
+timeout 1 >nul
+color 12
+cls
+echo lol
+timeout 1 >nul
+color d4
+cls
+echo lol
+timeout 1 >nul
+color 53
+cls
+echo lol
+timeout 1 >nul
+color 93
+cls
+echo lol
+timeout 1 >nul
+color c4
+cls
+echo lol
+timeout 1 >nul
+color 9a
+cls
+echo lol
+timeout 1 >nul
+color 5a
+cls
+echo lol
+timeout 1 >nul
+color 7e
+cls
+echo lol
+timeout 1 >nul
+color 0a
+cls
+echo lol
+timeout 1 >nul
+color 0e
+cls
+echo lol
+timeout 1 >nul
+color 02
+cls
+echo lol
+timeout 1 >nul
+color 8e
+cls
+echo lol
+timeout 1 >nul
+color d1
+cls
+echo lol
+timeout 1 >nul
+color f5
+cls
+echo lol
+timeout 1 >nul
+color 13
+cls
+echo lol
+timeout 1 >nul
+color 24
+cls
+echo lol
+timeout 1 >nul
+color 4c
+cls
+echo lol
+timeout 1 >nul
+color 50
+cls
+echo lol
+timeout 1 >nul
+color 52
+cls
+echo lol
+timeout 1 >nul
+color ac
+cls
+echo lol
+timeout 1 >nul
+color a1
+cls
+echo lol
+timeout 1 >nul
+color e8
+cls
+echo lol
+timeout 1 >nul
+color 3e
+cls
+echo lol
+timeout 1 >nul
+color a8
+cls
+echo lol
+timeout 1 >nul
+color f3
+cls
+echo lol
+timeout 1 >nul
+color db
+cls
+echo lol
+timeout 1 >nul
+color da
+cls
+echo lol
+timeout 1 >nul
+color e1
+cls
+echo lol
+timeout 1 >nul
+color a2
+cls
+echo lol
+timeout 1 >nul
+color b6
+cls
+echo lol
+timeout 1 >nul
+color b7
+cls
+echo lol
+timeout 1 >nul
+color 84
+cls
+echo lol
+timeout 1 >nul
+color a5
+cls
+echo lol
+timeout 1 >nul
+color 34
+cls
+echo lol
+timeout 1 >nul
+color 7c
+cls
+echo lol
+timeout 1 >nul
+color 80
+cls
+echo lol
+timeout 1 >nul
+color c2
+cls
+echo lol
+timeout 1 >nul
+color b8
+cls
+echo lol
+timeout 1 >nul
+color bf
+cls
+echo lol
+timeout 1 >nul
+color 0b
+cls
+echo lol
+timeout 1 >nul
+color c9
+cls
+echo lol
+timeout 1 >nul
+color a6
+cls
+echo lol
+timeout 1 >nul
+color 7d
+cls
+echo lol
+timeout 1 >nul
+color 27
+cls
+echo lol
+timeout 1 >nul
+color 29
+cls
+echo lol
+timeout 1 >nul
+color 04
+cls
+echo lol
+timeout 1 >nul
+color 76
+cls
+echo lol
+timeout 1 >nul
+color af
+cls
+echo lol
+timeout 1 >nul
+color 8d
+cls
+echo lol
+timeout 1 >nul
+color 14
+cls
+echo lol
+timeout 1 >nul
+color cb
+cls
+echo lol
+timeout 1 >nul
+color 65
+cls
+echo lol
+timeout 1 >nul
+color f6
+cls
+echo lol
+timeout 1 >nul
+color c7
+cls
+echo lol
+timeout 1 >nul
+color 8c
+cls
+echo lol
+timeout 1 >nul
+color 0f
+cls
+echo lol
+timeout 1 >nul
+color 15
+cls
+echo lol
+timeout 1 >nul
+color 25
+cls
+echo lol
+timeout 1 >nul
+color 0d
+cls
+echo lol
+timeout 1 >nul
+color 39
+cls
+echo lol
+timeout 1 >nul
+color 9d
+cls
+echo lol
+timeout 1 >nul
+color fc
+cls
+echo lol
+timeout 1 >nul
+color be
+cls
+echo lol
+timeout 1 >nul
+color 71
+cls
+echo lol
+timeout 1 >nul
+color 70
+cls
+echo lol
+timeout 1 >nul
+color 83
+cls
+echo lol
+timeout 1 >nul
+color 7a
+cls
+echo lol
+timeout 1 >nul
+color e0
+cls
+echo lol
+timeout 1 >nul
+color 81
+cls
+echo lol
+timeout 1 >nul
+color fd
+cls
+echo lol
+timeout 1 >nul
+color f0
+cls
+echo lol
+timeout 1 >nul
+color 05
+cls
+echo lol
+timeout 1 >nul
+color e4
+cls
+echo lol
+timeout 1 >nul
+color 89
+cls
+echo lol
+timeout 1 >nul
+color 59
+cls
+echo lol
+timeout 1 >nul
+color 87
+cls
+echo lol
+timeout 1 >nul
+color c6
+cls
+echo lol
+timeout 1 >nul
+color 26
+cls
+echo lol
+timeout 1 >nul
+color 1e
+cls
+echo lol
+timeout 1 >nul
+color 45
+cls
+echo lol
+timeout 1 >nul
+color ed
+cls
+echo lol
+timeout 1 >nul
+color ab
+cls
+echo lol
+timeout 1 >nul
+color 6b
+cls
+echo lol
+timeout 1 >nul
+color d0
+cls
+echo lol
+timeout 1 >nul
+color 85
+cls
+echo lol
+timeout 1 >nul
+color 7f
+cls
+echo lol
+timeout 1 >nul
+color ea
+cls
+echo lol
+timeout 1 >nul
+color 6d
+cls
+echo lol
+timeout 1 >nul
+color 86
+cls
+echo lol
+timeout 1 >nul
+color fe
+cls
+echo lol
+timeout 1 >nul
+color eb
+cls
+echo lol
+timeout 1 >nul
+color df
+cls
+echo lol
+timeout 1 >nul
+color 17
+cls
+echo lol
+timeout 1 >nul
+color 54
+cls
+echo lol
+timeout 1 >nul
+color d3
+cls
+echo lol
+timeout 1 >nul
+color e3
+cls
+echo lol
+timeout 1 >nul
+color cf
+cls
+echo lol
+timeout 1 >nul
+color c0
+cls
+echo lol
+timeout 1 >nul
+color d7
+cls
+echo lol
+timeout 1 >nul
+color 10
+cls
+echo lol
+timeout 1 >nul
+color 19
+cls
+echo lol
+timeout 1 >nul
+color 2f
+cls
+echo lol
+timeout 1 >nul
+color dc
+cls
+echo lol
+timeout 1 >nul
+color 6a
+cls
+echo lol
+timeout 1 >nul
+color 5f
+cls
+echo lol
+timeout 1 >nul
+color e6
+cls
+echo lol
+timeout 1 >nul
+color bd
+cls
+echo lol
+timeout 1 >nul
+color 62
+cls
+echo lol
+timeout 1 >nul
+color 2a
+cls
+echo lol
+timeout 1 >nul
+color 38
+cls
+echo lol
+timeout 1 >nul
+color d5
+cls
+echo lol
+timeout 1 >nul
+color b0
+cls
+echo lol
+timeout 1 >nul
+color 3c
+cls
+echo lol
+timeout 1 >nul
+color a0
+cls
+echo lol
+timeout 1 >nul
+color ca
+cls
+echo lol
+timeout 1 >nul
+color d9
+cls
+echo lol
+timeout 1 >nul
+color 1a
+cls
+echo lol
+timeout 1 >nul
+color 5d
+cls
+echo lol
+timeout 1 >nul
+color 3f
+cls
+echo lol
+timeout 1 >nul
+color cd
+cls
+echo lol
+timeout 1 >nul
+color f2
+cls
+echo lol
+timeout 1 >nul
+color ef
+cls
+echo lol
+timeout 1 >nul
+color 47
+cls
+echo lol
+timeout 1 >nul
+color 73
+cls
+echo lol
+timeout 1 >nul
+color 6e
+cls
+echo lol
+timeout 1 >nul
+color b3
+cls
+echo lol
+timeout 1 >nul
+color c3
+cls
+echo lol
+timeout 1 >nul
+color f1
+cls
+echo lol
+timeout 1 >nul
+color d2
+cls
+echo lol
+timeout 1 >nul
+color 07
+cls
+echo lol
+timeout 1 >nul
+color 91
+cls
+echo lol
+timeout 1 >nul
+color ce
+cls
+echo lol
+timeout 1 >nul
+color 68
+cls
+echo lol
+timeout 1 >nul
+color c8
+cls
+echo lol
+timeout 1 >nul
+color 30
+cls
+echo lol
+timeout 1 >nul
+color e7
+cls
+echo lol
+timeout 1 >nul
+color 2d
+cls
+echo lol
+timeout 1 >nul
+color 48
+cls
+echo lol
+timeout 1 >nul
+color 58
+cls
+echo lol
+timeout 1 >nul
+color 78
+cls
+echo lol
+timeout 1 >nul
+color c5
+cls
+echo lol
+timeout 1 >nul
+color 21
+cls
+echo lol
+timeout 1 >nul
+color 2e
+cls
+echo lol
+timeout 1 >nul
+color 7b
+cls
+echo lol
+timeout 1 >nul
+color 01
+cls
+echo lol
+timeout 1 >nul
+goto start
